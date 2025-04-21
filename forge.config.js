b8266b592b8b34b0c738e4def36321bb8adec7db
Aaron Santiago <me@aaron.work>
2025-04-21 19:29:58 -0400

    fix build issues on linuxz'

--- forge.config.js
+++ forge.config.js
@@ -10,15 +10,7 @@ module.exports = {
     {
       name: '@electron-forge/maker-zip',
       // platforms: ['darwin'],
-    },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
+    }
   ],
   plugins: [
     {
